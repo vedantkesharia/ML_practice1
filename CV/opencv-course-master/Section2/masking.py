a461863461b08307f7fae3ce8e5fc47383a46296
vedantkesharia <keshariavedant@gmail.com>
2024-01-29 22:03:36 +0530

    commit3 29th jan

--- CV/opencv-course-master/Section2/masking.py
+++ CV/opencv-course-master/Section2/masking.py
@@ -1,5 +1,7 @@
 #pylint:disable=no-member
 
+# Masking in computer vision refers to the process of using a binary image (referred to as a mask) to selectively modify or analyze certain parts of another image. The binary mask acts as a filter, specifying which pixels or regions of the original image should be considered or excluded in subsequent operations.
+
 import cv2 as cv
 import numpy as np
 
@@ -11,12 +13,14 @@ cv.imshow('Blank Image', blank)
 
 circle = cv.circle(blank.copy(), (img.shape[1]//2 + 45,img.shape[0]//2), 100, 255, -1)
 
-rectangle = cv.rectangle(blank.copy(), (30,30), (370,370), 255, -1)
+# rectangle = cv.rectangle(blank.copy(), (30,30), (370,370), 255, -1)
 
-weird_shape = cv.bitwise_and(circle,rectangle)
-cv.imshow('Weird Shape', weird_shape)
+# weird_shape = cv.bitwise_and(circle,rectangle)
+# cv.imshow('Weird Shape', weird_shape)
 
-masked = cv.bitwise_and(img,img,mask=weird_shape)
+masked = cv.bitwise_and(img,img,mask=circle) 
+# you can also try rectangle or weird shape in mask
+
 cv.imshow('Weird Shaped Masked Image', masked)
 
 cv.waitKey(0)
