d5964d40ec37d7efe28dab34c6847e1a5675118e
vedantkesharia <keshariavedant@gmail.com>
2024-01-29 20:52:14 +0530

    commit2 29th jan

--- CV/opencv-course-master/Section1/contours.py
+++ CV/opencv-course-master/Section1/contours.py
@@ -1,5 +1,15 @@
 #pylint:disable=no-member
 
+
+# Contours in an image refer to the outlines or boundaries of objects or shapes present in that image. These outlines are formed by connecting points along the boundary of an object with lines, creating a curve that represents the shape. In image processing and computer vision, extracting contours can be a crucial step in various applications such as object recognition, shape analysis, and image segmentation.
+
+# Contours can be used to identify and locate objects within an image, and they are particularly useful when dealing with binary or grayscale images. In a binary image, contours often represent the boundaries between regions of pixels with different intensity values.
+
+# The process of finding contours typically involves edge detection, which highlights significant changes in intensity or color within the image. Once the edges are detected, contours can be extracted by connecting the points along these edges.
+
+
+
+
 import cv2 as cv
 import numpy as np
 
--- CV/opencv-course-master/Section2/colour_spaces.py
+++ CV/opencv-course-master/Section2/colour_spaces.py
@@ -10,23 +10,23 @@ cv.imshow('Park', img)
 # plt.show()
 
 # BGR to Grayscale
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', gray)
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
 
 # BGR to HSV
-hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-cv.imshow('HSV', hsv)
+# hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+# cv.imshow('HSV', hsv)
 
 # BGR to L*a*b
-lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
-cv.imshow('LAB', lab)
+# lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+# cv.imshow('LAB', lab)
 
 # BGR to RGB
-rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-cv.imshow('RGB', rgb)
+# rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+# cv.imshow('RGB', rgb)
 
 # HSV to BGR
-lab_bgr = cv.cvtColor(lab, cv.COLOR_LAB2BGR)
-cv.imshow('LAB --> BGR', lab_bgr)
+# lab_bgr = cv.cvtColor(lab, cv.COLOR_LAB2BGR)
+# cv.imshow('LAB --> BGR', lab_bgr)
 
 cv.waitKey(0)
