d296b5afa677c277014b70f4a5cbb86233644e69
vedantkesharia <keshariavedant@gmail.com>
2024-03-16 12:47:28 +0530

    commit3 16th march

--- NLP/BagofWords.py
+++ NLP/BagofWords.py
@@ -1,19 +1,29 @@
 import nltk
-from nltk.stem import PorterStemmer
-from nltk.corpus import stopwords
 
 paragraph = """In 3000 years of our history, people from all over the world have been drawn to India's rich tapestry. This legacy of exchange inspires my three visions for India's future. Firstly, I see India as a global leader in innovation, drawing on its scientific heritage and youthful population to tackle pressing challenges  like climate change and sustainable development. Secondly, I envision India as a vibrant hub of cultures, where ancient traditions live alongside contemporary expressions, creating a unique and dynamic society. Finally, I dream of India as a beacon of peace and collaboration, fostering understanding and cooperation between nations, much like the famous scholars and traders who traversed the Silk Road centuries ago. These are just some of the ways India can continue to inspire the world in the next 3,000 years.""" 
 
-sentences = nltk.sent_tokenize(paragraph)
+#Cleaning the texts
 
+import re  #regular expression
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+lemmatizer = WordNetLemmatizer()
 stemmer = PorterStemmer()
-
-# Stemming
+sentences = nltk.sent_tokenize(paragraph)
+corpus = [] #after cleaning we will store it in this list
 for i in range(len(sentences)):
-    words = nltk.word_tokenize(sentences[i])
-    words = [stemmer.stem(word) for word in words if word not in set(stopwords.words('english'))]
-    sentences[i] = ' '.join(words)
+    review = re.sub('[^a-zA-Z]', ' ', sentences[i])
+    review = review.lower()
+    review = review.split()
+    review = [lemmatizer.lemmatize(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
 
-#we removed the stopwords here
-
-print(sentences)
+print(corpus)
+#Creating Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+print(X)
