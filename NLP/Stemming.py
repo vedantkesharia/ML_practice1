e125500eb3004a60b42ba954c1a44179b5e008bd
vedantkesharia <keshariavedant@gmail.com>
2024-03-16 11:31:22 +0530

    commit2 16th march

---
+++ NLP/Stemming.py
@@ -0,0 +1,17 @@
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+
+paragraph = """In 3000 years of our history, people from all over the world have been drawn to India's rich tapestry. This legacy of exchange inspires my three visions for India's future. Firstly, I see India as a global leader in innovation, drawing on its scientific heritage and youthful population to tackle pressing challenges  like climate change and sustainable development. Secondly, I envision India as a vibrant hub of cultures, where ancient traditions live alongside contemporary expressions, creating a unique and dynamic society. Finally, I dream of India as a beacon of peace and collaboration, fostering understanding and cooperation between nations, much like the famous scholars and traders who traversed the Silk Road centuries ago. These are just some of the ways India can continue to inspire the world in the next 3,000 years.""" 
+
+sentences = nltk.sent_tokenize(paragraph)
+
+stemmer = PorterStemmer()
+
+# Stemming
+for i in range(len(sentences)):
+    words = nltk.word_tokenize(sentences[i])
+    words = [stemmer.stem(word) for word in words if word not in set(stopwords.words('english'))]
+    sentences[i] = ' '.join(words)
+
+print(sentences)
